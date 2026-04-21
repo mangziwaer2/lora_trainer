8e737d77d912ce00974621cbf79aa208936a8890
mangziwaer <2308198415@qq.com>
2026-04-21 21:26:35 +0800

    fix diffusion trainer best-save signature

--- extensions_built_in/sd_trainer/DiffusionTrainer.py
+++ extensions_built_in/sd_trainer/DiffusionTrainer.py
@@ -307,9 +307,15 @@ class DiffusionTrainer(SDTrainer):
         self.maybe_stop()
         self.update_status("running", "Training")
 
-    def save(self, step=None):
+    def save(self, step=None, save_dir=None, name_suffix='', save_optimizer=True, cleanup_old=True):
         self.maybe_stop()
         self.update_status("running", "Saving model")
-        super().save(step)
+        super().save(
+            step=step,
+            save_dir=save_dir,
+            name_suffix=name_suffix,
+            save_optimizer=save_optimizer,
+            cleanup_old=cleanup_old,
+        )
         self.maybe_stop()
         self.update_status("running", "Training")
